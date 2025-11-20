5a42acebb18bc376cb5ace2212d594495229b43c
Hytalo Bassi <hytalo-bassi@protonmail.com>
2025-11-20 13:47:53 -0400

    refactor: adds key for div

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { ref } from 'vue'
-import Discipline from './discipline.js';
+import { Discipline } from './discipline.js';
 
 export default {
   setup() {
@@ -20,3 +20,19 @@ export default {
     return { numero_semestres, getDisciplinesBySemester };
   }
 }
+
+// Observacao: para conseguir os dados do curso, troque codigo pelo seu codigo do curso:
+// curl 'https://graduacao.ufms.br/portal/matriz/get-pre-requisitos/<codigo>' \
+//   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0' \
+//   -H 'Accept: application/json' \
+//   -H 'Accept-Language: en-US,en;q=0.5' \
+//   -H 'Accept-Encoding: gzip, deflate, br, zstd' \
+//   -H 'X-Requested-With: XMLHttpRequest' \
+//   -H 'Sec-GPC: 1' \
+//   -H 'Connection: keep-alive' \
+//   -H 'Referer: https://graduacao.ufms.br/cursos/<codigo>/matriz' \
+//   -H 'Sec-Fetch-Dest: empty' \
+//   -H 'Sec-Fetch-Mode: cors' \
+//   -H 'Sec-Fetch-Site: same-origin' \
+//   -H 'Pragma: no-cache' \
+//   -H 'Cache-Control: no-cache'
--- discipline.js
+++ discipline.js
@@ -1,6 +1,25 @@
-export default class Discipline {
-  constructor(nome, carga_horaria) {
-    this.nome = nome;
+export const TipoCurso = Object.freeze({
+  Obrigatoria: 0,
+  Optativa: 1,
+});
+
+export class Discipline {
+  constructor(apelido, carga_horaria, id_curso, tipo, semestre, pre_requisitos, requisitoDe) {
+    this.apelido = apelido;
     this.carga_horaria = carga_horaria;
+
+    this.id_curso = id_curso;
+    if (!id_curso) this.id_curso = Math.floor(Math.random() * 10**6) + 1;
+
+    this.tipo = tipo;
+    if (!tipo) this.tipo = TipoCurso.Obrigatoria;
+    
+    this.semestre = semestre;
+
+    this.pre_requisitos = pre_requisitos;    
+    if (!pre_requisitos) this.pre_requisitos = [];
+
+    this.requisitoDe = requisitoDe;
+    if (!requisitoDe) this.requisitoDe = [];
   }
 }

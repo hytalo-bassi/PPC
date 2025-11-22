995caa1652cee0c2ffe3a3a2e599f383883e8602
Hytalo Bassi <hytalo-bassi@protonmail.com>
2025-11-22 19:11:01 -0400

    refactor: utiliza as classes do tailwindcss no PPC.css; escreve componentes SemestersScreen e DisciplineCard

---
+++ src/components/DisciplineCard.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['discipline'])
+</script>
+
+<template>
+    <div class="discipline">
+        <h1 class="font-semibold text-[12px]/[12px] first-letter:uppercase">{{ discipline.apelido }}</h1>
+        <p class="hidden">CH: {{ discipline.carga_horaria }} horas</p>
+    </div>
+</template>
---
+++ src/components/SemestersScreen.vue
@@ -0,0 +1,19 @@
+<script setup>
+import DisciplineCard from './DisciplineCard.vue';
+defineProps(['semesters'])
+</script>
+
+<template>
+    <div class="flex-1" v-for="i in semesters.length">
+        <div class="rounded-full text-center border border-white/50 bg-black/20 h-6 mb-2">
+            <span class="text-sm">{{ i }}</span>
+        </div>
+        <div class="flex justify-around flex-col h-full">
+            <DisciplineCard
+             v-for="discipline in semesters[i -1]"
+             :key="discipline.id"
+             :discipline="discipline"
+            />
+        </div>
+    </div>
+</template>

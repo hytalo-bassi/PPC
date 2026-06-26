693699e1e902917b4ecc20097bc7d418036d0e89
Hytalo Bassi <hytalombassi@gmail.com>
2026-06-26 20:45:20 +0000

    little changes to vite.config.js and App.test

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-/// <reference path="./tsconfig.node.json" />
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
+      "@": resolve(__dirname, "src"),
+    },
   },
-  publicDir: 'public',
+  publicDir: "public",
   server: {
     port: 8000,
-    open: true
-  }
+    open: true,
+  },
 });

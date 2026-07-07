6a3401dd01e4c39db1c2244bc426564f200ff1c4
hytalo-bassi <hytalombassi@gmail.com>
2026-07-07 00:34:41 +0000

    fix: update API endpoint in load_json function to use dynamic base URL

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+export default defineConfig({
+  base: "/PPC/",
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  publicDir: "public",
+  server: {
+    port: 8000,
+    open: true,
+    host: "0.0.0.0", // importante para desenvolvimento remoto
+  },
+});
